7ce649ccfa561358bba517433d80795fd5ca719b
christy313 <christycreativity@gmail.com>
2022-09-26 19:19:54 -0400

    Updates

---
+++ precache-manifest.0d390d8994ed6a96eec78f1a3f733ea1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2576c085b5c1b831e2b4dfc743fff56b",
+    "url": "/react15-project13/index.html"
+  },
+  {
+    "revision": "3f912749008eaeae0fe8",
+    "url": "/react15-project13/static/css/main.9dbdac49.chunk.css"
+  },
+  {
+    "revision": "2cc9e4f811c19838413e",
+    "url": "/react15-project13/static/js/2.e7a20039.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react15-project13/static/js/2.e7a20039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f912749008eaeae0fe8",
+    "url": "/react15-project13/static/js/main.6e2a6129.chunk.js"
+  },
+  {
+    "revision": "c46fa8b663c0e3cf2deb",
+    "url": "/react15-project13/static/js/runtime-main.a2f39c4f.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/react15-project13/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/react15-project13/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/react15-project13/static/media/phone.58d7e3d6.svg"
+  }
+]);
